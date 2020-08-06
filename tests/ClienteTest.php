7ea89bd3d09a7a6ae8b492cba02eda64570433d7
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-08-06 00:28:38 -0500

    Creacion de test cliente
    TEst de clietne

---
+++ tests/ClienteTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\InvalidArgumentException;
+use App\Models\Cliente;
+
+final class ClienteTest extends TestCase
+{
+    public function testSiPodemosGuardarCliente()
+    {
+        $cliente = new Cliente;
+
+        $cliente->CLIdni='77127218';
+        $cliente->CLInombre='Jhosmell';
+        $cliente->CLIapellido='Alfaro';
+        $cliente->CLIcelular='952614602';
+        $cliente->CLIemail='jhoalfarom@upt.pe';
+        $cliente->CLIclave='123456';      
+
+        $result = $cliente->guardar($cliente);
+
+        $this->assertTrue($result, true);
+    }
+
+    public function testListar()
+    {
+        $cliente = new Cliente;
+        $result = $cliente->listar();
+
+        $this->assertIsArray($result, 'No se pudo listar clientes.');
+    }
+
+    public function testSiPodemosObtenerPorId()
+    {
+    	$id = '111';
+        $cliente = new Cliente;
+
+        $result = $cliente->obtener($id);
+
+        $this->assertEquals($result->CLIdni, $id);
+    }
+
+
+}
